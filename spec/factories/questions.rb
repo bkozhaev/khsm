ed94098ff73852fb0ede8b951b96ce21ea9cef2e
bkozhaev <bkozhaev@gmail.com>
2018-06-14 11:26:01 +0400

    deleted idea files

--- spec/factories/questions.rb
+++ spec/factories/questions.rb
@@ -1,6 +1,9 @@
 FactoryBot.define do
   factory :question do
     answer1 {"#{rand(2001)}"}
+    answer2 {"#{rand(2001)}"}
+    answer3 {"#{rand(2001)}"}
+    answer4 {"#{rand(2001)}"}
 
     sequence(:text) {|n| "В каком году была косм. одиссея #{n}"}
 
--- spec/rails_helper.rb
+++ spec/rails_helper.rb
@@ -1,11 +1,20 @@
 # This file is copied to spec/ when you run 'rails generate rspec:install'
-require 'spec_helper'
 ENV['RAILS_ENV'] ||= 'test'
 require File.expand_path('../../config/environment', __FILE__)
 # Prevent database truncation if the environment is production
 abort("The Rails environment is running in production mode!") if Rails.env.production?
+require 'spec_helper'
 require 'rspec/rails'
 # Add additional requires below this line. Rails is not loaded until this point!
+
+# специальные "матчеры" - методы, удобные для тестирования валидаций
+Shoulda::Matchers.configure do |config|
+  config.integrate do |with|
+    with.test_framework :rspec
+    with.library :rails
+  end
+end
+
 
 # Requires supporting ruby files with custom matchers and macros, etc, in
 # spec/support/ and its subdirectories. Files matching `spec/**/*_spec.rb` are
@@ -22,13 +31,7 @@ require 'rspec/rails'
 #
 # Dir[Rails.root.join('spec/support/**/*.rb')].each { |f| require f }
 
-Shoulda::Matchers.configure do |config|
-  config.integrate do |with|
-    with.text_framework :rspec
-    with.library :rails
-  end
-end
-# Checks for pending migrations and applies them before tests are run.
+# Checks for pending migration and applies them before tests are run.
 # If you are not using ActiveRecord, you can remove this line.
 ActiveRecord::Migration.maintain_test_schema!
 
@@ -41,7 +44,10 @@ RSpec.configure do |config|
   # instead of true.
   config.use_transactional_fixtures = true
 
-  config.include Devise::TextHelpers, type: :controller
+  # Подключаем девайзовые классы для использвоания в тестах
+  # https://github.com/plataformatec/devise#test-helpers
+  # https://github.com/plataformatec/devise/issues/4133
+  config.include Devise::TestHelpers, type: :controller
   config.include Devise::TestHelpers, type: :view
 
   # RSpec Rails can automatically mix in different behaviours to your tests
