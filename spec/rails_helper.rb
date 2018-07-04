c9c73af260ce22794abb7377327abe373af11632
bkozhaev <bkozhaev@gmail.com>
2018-07-04 19:18:58 +0400

    bdd integral test

--- spec/rails_helper.rb
+++ spec/rails_helper.rb
@@ -67,6 +67,10 @@ RSpec.configure do |config|
 
   # Filter lines from Rails gems in backtraces.
   config.filter_rails_from_backtrace!
+
+  config.include Warden::Test::Helpers, type: :feature
   # arbitrary gems may also be filtered via:
   # config.filter_gems_from_backtrace("gem name")
 end
+
+Capybara.asset_host = "http://localhost:3000"
